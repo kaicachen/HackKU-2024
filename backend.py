d996b65e60dbeea35e86daa09102d94193c0571c
LandonPyko <pykomar2@gmail.com>
2024-04-12 22:45:14 -0500

    takes in user info

--- backend.py
+++ backend.py
@@ -10,15 +10,19 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'TN9VVQ%YPHu45YLftak$'
 app.config['MYSQL_DB'] = 'mental_health'
 
+mysql = MySQL(app)
+
 
 @app.route("/")
 def hello_world():
     return render_template('frontpage.html')
 
-@app.route("/process_form")
+@app.route("/process_form", methods = ["POST"])
 def process():
     if request.method == "POST":
-        name = request.form.get()
+        name = request.form.get("name")
+
+        return "Your name is: " + name
 
 
 @app.route("/mood")
