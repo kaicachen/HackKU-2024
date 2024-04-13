5228227752d953dabc1fe4c5a13ea0b3b44ba697
Binkly <kaicachen@gmail.com>
2024-04-13 11:20:08 -0500

    removed carousel on index.html

--- backend.py
+++ backend.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
-
-app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+app = Flask(__name__)
 
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -13,16 +12,26 @@ app.config['MYSQL_DB'] = 'mental_health'
 mysql = MySQL(app)
 
 
+
+
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('frontpage.html')
 
 @app.route("/process_form", methods = ["POST"])
 def process():
     if request.method == "POST":
-        name = request.form.get("name")
+        username = request.form.get("username")
+        date = request.form.get("date")
 
-        return "Your name is: " + name
+        cursor = mysql.connection.cursor()
+        query = "SELECT note FROM logs WHERE username = %s AND date = %s"
+
+        cursor.execute(query,(username,date,))
+        result = cursor.fetchall()
+        cursor.close()
+
+        return str(result[0][0])
 
 
 @app.route("/mood")
