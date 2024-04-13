89ce02b6d03d864b4e30d4f8408e658e13a5d0b5
LandonPyko <pykomar2@gmail.com>
2024-04-12 21:54:07 -0500

    added support for html templates

--- backend.py
+++ backend.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello,World<p>"
+    return render_template('frontpage.html')
+
+@app.route("/mood")
+def mood():
+    return render_template('mood.html')
+    
 
 
 if __name__ == '__main__':
