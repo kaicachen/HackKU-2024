6b404f605f75f07178444a4c6db088a8fbb8d6ad
LandonPyko <pykomar2@gmail.com>
2024-04-12 22:37:29 -0500

    trying to connect mysql to flask

--- backend.py
+++ backend.py
@@ -1,12 +1,25 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+
 
 app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'TN9VVQ%YPHu45YLftak$'
+app.config['MYSQL_DB'] = 'mental_health'
 
 
 @app.route("/")
 def hello_world():
     return render_template('frontpage.html')
+
+@app.route("/process_form")
+def process():
+    if request.method == "POST":
+        name = request.form.get()
+
 
 @app.route("/mood")
 def mood():
