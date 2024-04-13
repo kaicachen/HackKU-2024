33df81d1aeb21994c87b46b3884abb01ddcd4b2d
LandonPyko <pykomar2@gmail.com>
2024-04-13 13:19:19 -0500

    added a login page

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -17,6 +17,11 @@ mysql = MySQL(app)
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    return render_template("login.html")
+
 
 @app.route("/process_form", methods = ["POST"])
 def process():
