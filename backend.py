a95d5039e0e9f8fed2bb7eb98ef9921355eb933c
LandonPyko <pykomar2@gmail.com>
2024-04-13 15:12:46 -0500

    W login

--- backend.py
+++ backend.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.secret_key = "hello"
+
+app.config['SESSION_COOKIE_NAME'] = 'session'  # Name of the session cookie
+app.config['SESSION_COOKIE_HTTPONLY'] = True  # Limit cookie access to HTTP requests
+app.config['SESSION_COOKIE_SECURE'] = True  # Only send cookie over HTTPS
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'  # SameSite policy for cookies
+
+
 
 
 app.config['MYSQL_HOST'] = 'localhost'
@@ -13,10 +21,40 @@ mysql = MySQL(app)
 
 
 
-
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    if session.get("username") is None:
+        return render_template("login.html")
+        
+    else:
+        return render_template("login_success.html")
+
+@app.route("/login_info", methods = ["GET","POST"])
+def login_info():
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        session["username"] = username
+        session["password"] = password
+        return redirect(url_for("login"))   
+
+    return render_template("login_success.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
+    
+
 
 @app.route("/process_form", methods = ["POST"])
 def process():
@@ -36,7 +74,15 @@ def process():
 
 @app.route("/mood")
 def mood():
-    return render_template('mood.html')
+    return render_template("mood.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
     
 
 
