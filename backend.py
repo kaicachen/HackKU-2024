a40636f4e83afa3eab753ab6540818d94714590d
Binkly <kaicachen@gmail.com>
2024-04-14 00:55:44 -0500

    missing one edge case and lots of sleep

--- backend.py
+++ backend.py
@@ -21,7 +21,7 @@ app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'  # SameSite policy for cookies
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'TN9VVQ%YPHu45YLftak$'
+app.config['MYSQL_PASSWORD'] = 'Terabyter47m!'
 app.config['MYSQL_DB'] = 'mental_health'
 
 mysql = MySQL(app)
@@ -198,6 +198,8 @@ def plot():
     cursor.execute("SELECT date, score FROM logs WHERE username = %s", (username,))
     rows = cursor.fetchall()
     cursor.close()
+    if rows is None:
+        return render_template("no_result.html")
 
     # Extract dates and scores
     dates = [row[0] for row in rows]
